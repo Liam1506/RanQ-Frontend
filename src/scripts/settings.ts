5a91d2d044b7292e2262279016208225dab1d240
Liam Wittig <liam.wittig@sap.com>
2026-05-28 13:19:45 +0200

    minor fixes

--- src/scripts/settings.ts
+++ src/scripts/settings.ts
@@ -1,4 +1,8 @@
 import { API } from "../config/api";
+import { getCookie } from "../utils/cookies";
+
+const userId = getCookie("userId");
+if (!userId) window.location.replace("/login");
 
 // display account information
 // add functionality to delete account, other settings
